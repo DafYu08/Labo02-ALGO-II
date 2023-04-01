368e665328fff8e4ee1afd39916aa62354f2f1e8
DafYu08 <104454993+DafYu08@users.noreply.github.com>
2023-04-01 20:26:48 -0300

    Update Geometria.cpp

--- Geometria.cpp
+++ Geometria.cpp
@@ -1,12 +1,9 @@
 #include <iostream>
 #include <ostream>
-
 using namespace std;
-
 using uint = unsigned int;
 
 // Ejercicio 1
-
 class Rectangulo {
     public:
         Rectangulo(uint alto, uint ancho);
@@ -14,20 +11,16 @@ class Rectangulo {
         uint ancho();
         float area();
         //int operator*(Rectangulo mult); PREGUNTAR SI HACE FALTA
-
     private:
         int alto_;
         int ancho_;
-
 };
-
 Rectangulo::Rectangulo(uint alto, uint ancho) :
     alto_(alto), ancho_(ancho){}
 
 uint Rectangulo::alto() {
     return alto_;
 }
-
 uint Rectangulo::ancho() {
     return ancho_;
 }
@@ -39,38 +32,32 @@ float Rectangulo::area() {
 } PREGUNTAR SI HAY QUE AGREGAR EL MULTIPLICAR*/
 
 // Ejercicio 2
-
 class Elipse{
 public:
     Elipse(uint a, uint b);
     uint a();
     uint b();
     float area();
-
 private:
     uint r_a;
     uint r_b;
     float PI = 3.14;
 };
-
 Elipse::Elipse (uint a, uint b):
     r_a(a), r_b(b), PI(){}
 
 uint Elipse::a(){
     return r_a;
 }
-
 uint Elipse::b(){
     return r_b;
 }
-
 float Elipse::area(){
     return r_a * r_b * PI;
 }
 
 
 // Ejercicio 3
-
 class Cuadrado {
     public:
         Cuadrado(uint lado);
@@ -87,13 +74,11 @@ Cuadrado::Cuadrado(uint lado): r_(lado, lado) {}
 uint Cuadrado::lado() {
     return lado_;
 }
-
 float Cuadrado::area() {
     return lado_ * lado_;
 }
 
 // Ejercicio 4
-
 class Circulo{
 public:
     Circulo(float radio);
@@ -122,18 +107,13 @@ float Circulo::area(){
     return  radio_ * radio_ * PI;
 }
 
-
-
 // Ejercicio 5
-
 ostream& operator<<(ostream& os, Elipse e) {
     os << "Elipse(" << e.a() << ", " << e.b() << ")";
     return os;
 }
 
 // Ejercicio 6
-
-
 ostream& operator<<(ostream& os, Cuadrado q) {
     os << "Cuadrado(" << q.lado() << ")";
     return os;
